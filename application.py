6b64ebb1dd56beb2b381d7e7d0370c24f953d74a
Megan Elizabeth Petroski <megan.petroski@pb.com>
2018-04-26 15:33:28 +0000

    added templates for new pages

--- application.py
+++ application.py
@@ -1,6 +1,22 @@
-from flask import Flask
+import os
+import calendar
+
+from flask import Flask, render_template, request
+
+from cs50 import SQL
+
+# Configure application
 app = Flask(__name__)
 
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///heartbeat.db")
+
 @app.route("/")
-def main():
-    return "The website, it works!!"
+def index():
+    # sample a record from heartbeat.db
+    sample = db.execute("SELECT * FROM results")
+
+    #cal = (calendar.calendar(2018))
+
+    return render_template("home.html", sample=sample)
+    # return "The website, it works!!"
