c3826c74a82ea7d7069f952f27bd41f216794925
Megan Elizabeth Petroski <megan.petroski@pb.com>
2018-05-10 14:25:00 +0000

    done

--- application.py
+++ application.py
@@ -20,20 +20,24 @@ app = Flask(__name__)
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///heartbeat.db")
 
+
 @app.route("/")
 def home():
 
     return render_template("home.html")
+
 
 @app.route("/about_us")
 def about_us():
 
     return render_template("about_us.html")
 
+
 @app.route("/query")
 def query():
 
     return render_template("query.html")
+
 
 @app.route("/result")
 def result():
@@ -44,7 +48,8 @@ def result():
     buildnum_ret = request.args.get('runs')
 
     # data to download
-    download = db.execute("SELECT buildnumber, product, region, attribution, query, result_text, result_count FROM data WHERE buildnumber = :buildnumber", buildnumber=buildnum_ret)
+    download = db.execute(
+        "SELECT buildnumber, product, region, attribution, query, result_text, result_count FROM data WHERE buildnumber = :buildnumber", buildnumber=buildnum_ret)
     htmlcsv = db.execute("SELECT * FROM data WHERE buildnumber = :buildnumber", buildnumber=buildnum_ret)
 
     # dynamically attach build number to csv output
@@ -58,10 +63,12 @@ def result():
 
     return render_template("result.html", runs=runs, htmlcsv=htmlcsv)
 
+
 @app.route("/compare_home")
 def compare_home():
 
     return render_template("compare_home.html")
+
 
 @app.route("/compare", methods=["POST"])
 def compare():
@@ -86,11 +93,13 @@ def compare():
     # Output comparison
     return render_template("compare.html", file1=file1, file2=file2)
 
+
 @app.errorhandler(HTTPException)
 def errorhandler(error):
     """Handle errors"""
     # resource: pset6 similiarities less version
     return render_template("error.html", error=error), error.code
+
 
 # resource: pset6 similiarities less version
 # https://github.com/pallets/flask/pull/2314
